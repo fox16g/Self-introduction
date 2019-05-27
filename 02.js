e35a3aabf2e68c18d47085d29af2d5fb6a2d9ab6
fox16g <to4431ot@gmail.com>
2019-05-27 22:33:33 +0900

    add BootSnipp

--- 02.js
+++ 02.js
@@ -6,3 +6,17 @@ $('.cebu').slick({
   cssEase: 'linear'
 });
 			
+//スムーズスクロール
+$('.btn-home').on('click',function(){
+	$('body,html').animate({scrollTop:0},1000);
+});
+
+// $(function(){
+//    $('a[href^="#"]').click(function() {
+//       var href= $(this).attr("href");
+//       var target = $(href == "#" || href == "" ? 'html' : href);
+//       var position = target.offset();
+//       $('body,html').animate({scrollTop:position}, 400, 'swing');
+//       return false;
+//    });
+// });
